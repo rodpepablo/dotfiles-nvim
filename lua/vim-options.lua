33bd5e15f115584e3f9e81ff8415c2b69fd1eba8
rodpepablo <rodpepablo@gmail.com>
2025-04-10 12:21:00 +0200

    ADD: initial configuration -> theme, lsp, lualine, neo-tree, telescope, tree-sitter

---
+++ lua/vim-options.lua
@@ -0,0 +1,11 @@
+-- Indentation
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+-- Leader
+vim.g.mapleader = ","
+vim.g.maplocalleader = "\\"
+
+
