9114c90c66b38d9b5a2d5edf7c9956e51e3edf18
rodpepablo <rodpepablo@gmail.com>
2025-04-11 23:38:57 +0200

    ADD: basic vim settings for editor

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,9 +1,23 @@
--- Indentation
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
--- Leader
 vim.g.mapleader = ","
 vim.g.maplocalleader = "\\"
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
